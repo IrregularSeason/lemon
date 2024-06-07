c3e5c43894c7305be1a11421c95070ee6192ec12
seasonHL <lingkongsiji@qq.com>
2024-06-07 22:00:52 +0800

    var

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -11,14 +11,14 @@ import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
+  constructor(private readonly uploadService: UploadService) { }
   @Post('avatar')
   @UseInterceptors(FileInterceptor('avatar'))
   uploadAvatar(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
     return this.uploadService.updateAvatar(file, req);
   }
   @Post('image')
-  @UseInterceptors(FileInterceptor('avatar'))
+  @UseInterceptors(FileInterceptor('image'))
   uploadImage(@UploadedFile() file: Express.Multer.File) {
     return this.uploadService.updateImage(file);
   }
